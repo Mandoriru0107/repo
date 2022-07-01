66fdf8acdb79c6846d8165568a9a59b191cbdbf6
Mandoriru0107 <79515833+Mandoriru0107@users.noreply.github.com>
2022-07-01 13:48:14 +0900

    Update main.sh
    SSHを設定するかどうかのオプションを設定。
    ファイルを書き換える前に"./backups/"に書き換えるファイルをコピーするように設定。

--- main.sh
+++ main.sh
@@ -2,18 +2,29 @@
 
 sudo pacman -Syy
 sudo pacman -Syu
+mkdir ./backups
 echo "Do you want to install packages required for build?(y|n)"
 read install
+echo "Do you want to set ssh?(y|n)"
+read ssh
+if [ $ssh == "y" ] || [ $ssh == "Y" ] || [ $ssh == "yes" ] || [ $ssh == "Yes" ]; then
+    echo "Do you want to set ssh auth(publickey)(y|n)"   
+    read auth
+fi
+
 if [ $install == "y" ] || [ $install == "Y" ] || [ $install == "yes" ] || [ $install == "Yes" ]; then
     sudo pacman -S base-devel git wget vim cmake llvm clang go htop ufw
     git clone https://aur.archlinux.org/yay.git &&cd yay &&makepkg -si &&cd .. &&rm -rf yay
 fi
+
 if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
     sudo chmod 700 ~/.ssh
 fi
-echo "Do you want to set ssh auth?(PubkeyAuthentication)(y|n)"
-read auth
+
+if [ $ssh == "y" ] || [ $ssh == "Y" ] || [ $ssh == "yes" ] || [ $ssh == "Yes" ]; then
+    sudo pacman -S openssh
+fi
 
 if [ $auth == "y" ] || [ $auth == "Y" ] || [ $auth == "yes" ] || [ $auth == "Yes" ]; then
     sudo pacman -S openssh
@@ -21,7 +32,7 @@ if [ $auth == "y" ] || [ $auth == "Y" ] || [ $auth == "yes" ] || [ $auth == "Yes
     read key
     echo $key >> ~/.ssh/authorized_keys
     sudo chmod 600 ~/.ssh/authorized_keys
-    sudo cp -r /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
+    sudo cp -r /etc/ssh/sshd_config ./backups/
     sudo sed -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
     sudo systemctl restart sshd
 fi
